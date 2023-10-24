6844bf2a876b0c42989a1642022bda3eedf327d8
aspen801 <boyko.bogdan45@gmail.com>
2023-10-24 22:29:10 +0300

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import './app.scss'
 import Header from './components/Header/Header'
-import Navbar from './components/Navbar/Navbar'
 import HomePage from './pages/HomePage/HomePage'
 
 function App() {
