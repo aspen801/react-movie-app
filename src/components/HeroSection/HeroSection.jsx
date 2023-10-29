887916e8bf523eb872295c0c8ecbf9231f0bd474
aspen801 <boyko.bogdan45@gmail.com>
2023-10-29 22:56:35 +0200

    reworked for consuming new api service

--- src/components/HeroSection/HeroSection.jsx
+++ src/components/HeroSection/HeroSection.jsx
@@ -1,33 +1,27 @@
 import React, { useEffect, useState } from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
 import { Autoplay } from "swiper/modules";
+import { getTrendingMedia } from "../../api/media.api";
 import MovieInfo from "./MovieInfo";
 import "swiper/css";
 import "./herosection.scss";
-
-const url = "https://api.themoviedb.org/3/trending/all/week?language=en-US";
-const options = {
-  method: "GET",
-  headers: {
-    accept: "application/json",
-    Authorization:
-      "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJhMDk4YzRhYjBmNTU2YzI0ZGNjM2VhMDdmZWJhNWFlMyIsInN1YiI6IjY1M2I4Mjc4NTE5YmJiMDBhYjY3Y2QxOCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.SNUHh2C-p7b7eW-KPGukdg8XPAJqVqfFXvAFN3RKCPY",
-  },
-};
 
 const HeroSection = () => {
   const [infos, setInfos] = useState([]);
 
   useEffect(() => {
-    fetch(url, options)
-      .then((res) => res.json())
-      .then((json) => {
-        setInfos(json.results);
-      })
-      .catch((err) => console.error("error:" + err));
+    const fetchData = async () => {
+      try {
+        const data = await getTrendingMedia();
+        setInfos(data.results);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+
+    fetchData();
   }, []);
 
-  console.log(typeof infos);
   return (
     <div className="hero__main-wrapper">
       <Swiper
