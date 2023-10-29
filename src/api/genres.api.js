efefc2f98e633dbf50c02b3f8f66478b0f84aa67
aspen801 <boyko.bogdan45@gmail.com>
2023-10-29 22:55:37 +0200

    added genres api file

---
+++ src/api/genres.api.js
@@ -0,0 +1,38 @@
+import tmdb from "./configs/tmbd.config";
+
+const getAllGenres = async () => {
+  try {
+    const movieGenresResponse = await fetch(
+      `${tmdb.baseUrl}/genre/movie/list?language=en`,
+      {
+        headers: {
+          Authorization: `Bearer ${tmdb.key}`,
+        },
+      }
+    );
+
+    const tvGenresResponse = await fetch(
+      `${tmdb.baseUrl}/genre/tv/list?language=en`,
+      {
+        headers: {
+          Authorization: `Bearer ${tmdb.key}`,
+        },
+      }
+    );
+
+    const movieGenres = await movieGenresResponse.json();
+    const tvGenres = await tvGenresResponse.json();
+
+    console.log(movieGenres);
+    console.log(tvGenres);
+
+    const combinedGenres = [...movieGenres.genres, ...tvGenres.genres];
+
+    return { genres: combinedGenres };
+  } catch (error) {
+    console.error("Error fetching data:", error);
+    throw error;
+  }
+};
+
+export { getAllGenres };
