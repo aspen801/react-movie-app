b2e1356977f0474c6967b212146d16d6f8049a62
aspen801 <boyko.bogdan45@gmail.com>
2023-10-29 22:55:05 +0200

    added tmdb api config

---
+++ src/api/configs/tmbd.config.js
@@ -0,0 +1,6 @@
+const tmbd = {
+  baseUrl: "https://api.themoviedb.org/3",
+  key: "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJhMDk4YzRhYjBmNTU2YzI0ZGNjM2VhMDdmZWJhNWFlMyIsInN1YiI6IjY1M2I4Mjc4NTE5YmJiMDBhYjY3Y2QxOCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.SNUHh2C-p7b7eW-KPGukdg8XPAJqVqfFXvAFN3RKCPY",
+};
+
+export default tmbd;
