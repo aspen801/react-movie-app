864bb04c494b790b84d0c4701559a07eb152c562
aspen801 <boyko.bogdan45@gmail.com>
2023-10-30 22:10:47 +0200

    reworked media api file

--- src/api/media.api.js
+++ src/api/media.api.js
@@ -1,29 +1,30 @@
 import tmdb from "./configs/tmbd.config";
 
-const getTrendingMedia = async () => {
-  const response = await fetch(
-    `${tmdb.baseUrl}/trending/all/week?language=en-US`,
-    {
-      headers: {
-        Authorization: `Bearer ${tmdb.key}`,
-      },
-    }
-  );
+const mediaEndpoints = {
+  trending: ({ mediaType, mediaTimeWindow }) => `trending/${mediaType}/${mediaTimeWindow}`,
+  list: ({ mediaType, mediaCategory }) => `${mediaType}/${mediaCategory}`,
+  detail: ({ mediaType, mediaId }) => `${mediaType}/detail/${mediaId}`,
+  search: ({ mediaType, query, page }) => `${mediaType}/search?query=${query}&page=${page}`,
+};
+
+const getTrendingMedia = async ({ mediaType, mediaTimeWindow }) => {
+  const response = await fetch(`${tmdb.baseUrl}/${mediaEndpoints.trending({ mediaType, mediaTimeWindow })}?language=en-US`, {
+    headers: {
+      Authorization: `Bearer ${tmdb.key}`,
+    },
+  });
 
   return await response.json();
 };
 
-const getMediaById = async () => {
-  const response = await fetch(
-    `${tmdb.baseUrl}/trending/all/week?language=en-US`,
-    {
-      headers: {
-        Authorization: `Bearer ${tmdb.key}`,
-      },
-    }
-  );
+const getMediaById = async ({ mediaType, mediaId }) => {
+  const response = await fetch(`${tmdb.baseUrl}/${mediaEndpoints.detail({ mediaType, mediaId })}`, {
+    headers: {
+      Authorization: `Bearer ${tmdb.key}`,
+    },
+  });
 
   return await response.json();
 };
 
-export { getTrendingMedia, getMedia };
+export { getTrendingMedia, getMediaById };
