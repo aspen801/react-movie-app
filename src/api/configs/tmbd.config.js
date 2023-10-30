6c44bd98682c13ca3ad45d70a8a9d64d9feacf63
aspen801 <boyko.bogdan45@gmail.com>
2023-10-30 22:11:17 +0200

    added media selectors to config

--- src/api/configs/tmbd.config.js
+++ src/api/configs/tmbd.config.js
@@ -1,6 +1,20 @@
 const tmbd = {
   baseUrl: "https://api.themoviedb.org/3",
   key: "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJhMDk4YzRhYjBmNTU2YzI0ZGNjM2VhMDdmZWJhNWFlMyIsInN1YiI6IjY1M2I4Mjc4NTE5YmJiMDBhYjY3Y2QxOCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.SNUHh2C-p7b7eW-KPGukdg8XPAJqVqfFXvAFN3RKCPY",
+  mediaType: {
+    all: "all",
+    movie: "movie",
+    tv: "tv",
+  },
+  mediaCategory: {
+    popular: "popular",
+    top_rated: "top_rated",
+    upcoming: "upcoming",
+  },
+  mediaTimeWindow: {
+    day: "day",
+    week: "week",
+  },
 };
 
 export default tmbd;
