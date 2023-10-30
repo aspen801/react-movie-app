ea2f804221d1bfc54012a2095f18314f01055ac3
aspen801 <boyko.bogdan45@gmail.com>
2023-10-30 22:12:49 +0200

    added reworked and renamed hero section

---
+++ src/components/HeroSection/HeroTrendingSlider.jsx
@@ -0,0 +1,42 @@
+import React, { useEffect, useState } from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Autoplay, Pagination } from "swiper/modules";
+import { getTrendingMedia } from "../../api/media.api";
+import MovieInfo from "./MovieInfo";
+import "swiper/css";
+import "swiper/css/pagination";
+
+import "./herotrendingslider.scss";
+
+const HeroTrendingSlider = ({ mediaType, mediaTimeWindow }) => {
+  const [infos, setInfos] = useState([]);
+  console.log(mediaTimeWindow);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const data = await getTrendingMedia({ mediaType, mediaTimeWindow });
+        setInfos(data.results);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  return (
+    <div className="hero__main-wrapper">
+      <Swiper modules={[Autoplay, Pagination]} className="swiper" speed={1000} autoplay={{ delay: 2500, disableOnInteraction: true }} pagination={{ clickable: true }}>
+        {infos.map((infoObject) => (
+          <SwiperSlide>
+            <MovieInfo info={infoObject} />
+          </SwiperSlide>
+        ))}
+      </Swiper>
+    </div>
+  );
+};
+export default HeroTrendingSlider;
+
+//TODO: add trending week/day switch button
