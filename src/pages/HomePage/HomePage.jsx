5e51d8a090161c85b1b929656bdd5050873713cf
aspen801 <boyko.bogdan45@gmail.com>
2023-10-30 15:33:48 +0200

    removed unused import statement

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -3,8 +3,6 @@ import "./homepage.scss";
 
 import HeroSection from "../../components/HeroSection/HeroSection";
 
-import background from "/assets/northman.jpg";
-
 const HomePage = () => {
   return (
     <div className="homepage__main-wrapper">
