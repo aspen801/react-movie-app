9c091c0465c5c7edf2d16423f871d720daef7511
aspen801 <boyko.bogdan45@gmail.com>
2023-11-04 23:26:01 +0200

    added home page layout

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -3,11 +3,24 @@ import "./homepage.scss";
 import tmbd from "../../api/configs/tmbd.config";
 
 import HeroTrendingSlider from "../../components/HeroSection/HeroTrendingSlider";
+import MediaSlider from "../../components/Media/MediaSlider";
+import MediaContainer from "../../components/Media/MediaContainer";
+
+//TODO: remove MediaContainer, its unnecessary
 
 const HomePage = () => {
   return (
     <div className="homepage__main-wrapper">
       <HeroTrendingSlider mediaType={tmbd.mediaType.all} mediaTimeWindow={tmbd.mediaTimeWindow.week} />
+      <MediaContainer name={"Now In Theaters"}>
+        <MediaSlider mediaType={tmbd.mediaType.movie} mediaCategory={tmbd.mediaCategory.now_playing} />
+      </MediaContainer>
+      <MediaContainer name={"Popular Tv Series"}>
+        <MediaSlider mediaType={tmbd.mediaType.tv} mediaCategory={tmbd.mediaCategory.popular} />
+      </MediaContainer>
+      <MediaContainer name={"Popular Movies"}>
+        <MediaSlider mediaType={tmbd.mediaType.movie} mediaCategory={tmbd.mediaCategory.popular} />
+      </MediaContainer>
     </div>
   );
 };
