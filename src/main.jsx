e7a008d09fa764b9813287c129752dc517336419
aspen801 <boyko.bogdan45@gmail.com>
2023-11-05 23:47:42 +0200

    added router provider

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,19 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App.jsx";
+import ScrollToTop from "./components/util/ScrollToTop.js";
 
-import { Provider } from 'react-redux'
-import store from './store/index.js'
+import { Provider } from "react-redux";
+import store from "./store/index.js";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <BrowserRouter>
+        <ScrollToTop />
+        <App />
+      </BrowserRouter>
     </Provider>
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
