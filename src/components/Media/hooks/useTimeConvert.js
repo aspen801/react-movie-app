2587ca1165587d7b6b6dd5e6e56f180fd65c5548
aspen801 <boyko.bogdan45@gmail.com>
2023-11-07 23:33:52 +0200

    moved hook to another directory

--- src/components/Media/hooks/useTimeConvert.js
+++
@@ -1,9 +0,0 @@
-const useTimeConvert = (num) => {
-  let hours = num / 60;
-  let rhours = Math.floor(hours);
-  let minutes = (hours - rhours) * 60;
-  let rminutes = Math.round(minutes);
-  return rhours + " h " + rminutes + " m";
-};
-
-export default useTimeConvert;
---
+++ src/hooks/useTimeConvert.js
@@ -0,0 +1,9 @@
+const useTimeConvert = (num) => {
+  const hours = num / 60;
+  const rhours = Math.floor(hours);
+  const minutes = (hours - rhours) * 60;
+  const rminutes = Math.round(minutes);
+  return `${rhours}h ${rminutes}m`;
+};
+
+export default useTimeConvert;
