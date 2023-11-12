808672e8c667444ef7f9d84a7ab89fc74e014fb2
aspen801 <boyko.bogdan45@gmail.com>
2023-11-12 22:53:16 +0200

    added chakra provider to app

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
+import { ChakraProvider } from "@chakra-ui/react";
 import App from "./App.jsx";
 import ScrollToTop from "./components/util/ScrollToTop.js";
 
@@ -9,11 +10,13 @@ import store from "./store/index.js";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <Provider store={store}>
-      <BrowserRouter>
-        <ScrollToTop />
-        <App />
-      </BrowserRouter>
-    </Provider>
+    <ChakraProvider>
+      <Provider store={store}>
+        <BrowserRouter>
+          <ScrollToTop />
+          <App />
+        </BrowserRouter>
+      </Provider>
+    </ChakraProvider>
   </React.StrictMode>
 );
