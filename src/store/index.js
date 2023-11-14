7adc1ba5e2bc1bf0f6236113981a3fec06f90ed5
aspen801 <boyko.bogdan45@gmail.com>
2023-11-14 22:15:09 +0200

    added loading redux slice

--- src/store/index.js
+++ src/store/index.js
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import themeReducer from "./slices/themeSlice";
+import loadingReducer from "./slices/loadingSlice";
 
 const store = configureStore({
   reducer: {
     theme: themeReducer,
+    loading: loadingReducer,
   },
 });
 
