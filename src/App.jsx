7535225c7ff83d360c63b6f05d69917c5b3c4ea5
aspen801 <boyko.bogdan45@gmail.com>
2023-11-16 23:03:21 +0200

    added upcoming page to app layout

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import HomePage from "./pages/HomePage/HomePage";
 import MoviesPage from "./pages/MoviesPage/MoviesPage";
 import SeriesPage from "./pages/SeriesPage/SeriesPage";
 import MediaPage from "./pages/MediaPage/MediaPage";
+import UpcomingPage from "./pages/UpcomingPage/UpcomingPage";
 import LoadingPage from "./pages/LoadingPage/LoadingPage";
 import Footer from "./components/Footer/Footer";
 
@@ -16,6 +17,7 @@ function App() {
       <LoadingPage />
       <Routes>
         <Route path={"/"} element={<HomePage />} />
+        <Route path={"/upcoming"} element={<UpcomingPage />} />
         <Route path={"/movies"} element={<MoviesPage />} />
         <Route path={"/series"} element={<SeriesPage />} />
         <Route path={"/details/:mediaType/:mediaId"} element={<MediaPage />} />
