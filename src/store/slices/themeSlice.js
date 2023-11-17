bb90e43e251675313bba21654ecbf3e3ab8b6450
aspen801 <boyko.bogdan45@gmail.com>
2023-11-17 21:58:41 +0200

    added toggle logic

--- src/store/slices/themeSlice.js
+++ src/store/slices/themeSlice.js
@@ -1,15 +1,15 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-const initialState = { theme: 'dark' };
+const initialState = { theme: "dark" };
 
 const themeSlice = createSlice({
   name: "theme",
   initialState,
   reducers: {
     toggleTheme(state, action) {
-        state.theme = action.payload
-    }
-  }
+      state.theme = state.theme === "light" ? "dark" : "light";
+    },
+  },
 });
 
 export const themeActions = themeSlice.actions;
