beb0054c7f5d37cee9c571fd1db57971c7367d51
aspen801 <boyko.bogdan45@gmail.com>
2023-11-29 22:53:38 +0200

    added user slice

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,15 @@
 import { configureStore } from "@reduxjs/toolkit";
 import themeReducer from "./slices/themeSlice";
 import loadingReducer from "./slices/loadingSlice";
+import authModalReducer from "./slices/authModalSlice";
+import userSlice from "./slices/userSlice";
 
 const store = configureStore({
   reducer: {
     theme: themeReducer,
     loading: loadingReducer,
+    auth: authModalReducer,
+    user: userSlice,
   },
 });
 
---
+++ src/store/slices/userSlice.js
@@ -0,0 +1,19 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+export const userSlice = createSlice({
+  name: "user",
+  initialState: {
+    user: null,
+    favorites: [],
+  },
+  reducers: {
+    setUser: (state, action) => {
+      state.user = action.payload;
+      console.log("writen in redux");
+    },
+  },
+});
+
+export const { setUser } = userSlice.actions;
+
+export default userSlice.reducer;
