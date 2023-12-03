78e4fb97db48bf60c70cc3a196340d4892b106f8
aspen801 <boyko.bogdan45@gmail.com>
2023-12-03 21:19:32 +0200

    added ripple buttons, auth logic, search input

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,37 +1,56 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { createRipples } from "react-ripples";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
+import { setUser } from "../../store/slices/userSlice";
+import RippleButton from "../UI/RippleButton/RippleButton";
+import SearchInput from "../Search/SearchInput/SearchInput";
+import { IconButton } from "@chakra-ui/react";
+import { logOut } from "../../firebase/auth";
+
 import "./header.scss";
 
 import { themeActions } from "../../store/slices/themeSlice";
+import { setAuthModalOpen, setAuthType } from "../../store/slices/authModalSlice";
 
-import textLogo from "/assets/textlogo.svg";
 import mLogo from "/assets/mlogo.svg";
-import boxLogo from "/assets/boxlogo.svg";
 import MoonIcon from "../../assets/moon.svg?react";
 import SunIcon from "../../assets/sun.svg?react";
 import SearchIcon from "../../assets/search.svg?react";
-
-const MyRipples = createRipples({
-  color: "rgba(255, 255, 255, 0.2)",
-  during: 800,
-});
-
-{
-  /* <div className='header__search-input'>
-          <img className='search-icon' src={search} alt="" />
-          <input type="text" placeholder="Search something..."/>
-        </div> */
-}
+import LogoutIcon from "../../assets/logout.svg?react";
+import CrossIcon from "../../assets/search-close.svg?react";
 
 const Header = () => {
   const dispatch = useDispatch();
-
+  const { user } = useSelector((state) => state.user);
   const { theme } = useSelector((state) => state.theme);
+  const [searchVisible, setSearchVisible] = useState(false);
+
+  let location = useLocation();
+
+  useEffect(() => {
+    console.log("User changed!");
+  }, [user]);
 
   const handleThemeChange = () => {
     dispatch(themeActions.toggleTheme());
+  };
+
+  const handleOpen = (authType) => {
+    dispatch(setAuthModalOpen(true));
+    dispatch(setAuthType(authType));
+  };
+
+  const handleLogout = () => {
+    dispatch(setUser(null));
+    logOut();
+  };
+
+  useEffect(() => {
+    setSearchVisible(false);
+  }, [location]);
+
+  const handleSearchVisible = () => {
+    setSearchVisible(!searchVisible);
   };
 
   return (
@@ -44,49 +63,47 @@ const Header = () => {
             </div>
           </Link>
         </div>
-
         <div className="header__nav-links">
-          <MyRipples className="ripple-container">
-            <Link to="/upcoming">
-              <button className="nav-button">Upcoming</button>
-            </Link>
-          </MyRipples>
-          <MyRipples className="ripple-container">
-            <Link to={"/movies"}>
-              <button className="nav-button">Movies</button>
-            </Link>
-          </MyRipples>
-          <MyRipples className="ripple-container">
-            <Link to={"/series"}>
-              <button className="nav-button">Series</button>
-            </Link>
-          </MyRipples>
-          <MyRipples className="ripple-container unactive">
-            <button className="nav-button">Persons</button>
-          </MyRipples>
+          <RippleButton buttonType={"secondary"} to={"/upcoming"}>
+            Upcoming
+          </RippleButton>
+          <RippleButton buttonType={"secondary"} to={"/movies"}>
+            Movies
+          </RippleButton>
+          <RippleButton buttonType={"secondary"} to={"/series"}>
+            Series
+          </RippleButton>
         </div>
       </div>
 
       <div className="header__right-menu">
         <div className="header__right-menu-search">
-          <button>
-            <SearchIcon className="icon" />
-          </button>
+          <button onClick={handleSearchVisible}>{searchVisible ? <CrossIcon className="icon" /> : <SearchIcon className="icon" />}</button>
         </div>
         <div className="header__right-menu-theme-toggle">
           <button onClick={handleThemeChange}>{theme === "dark" ? <MoonIcon className="icon" /> : <SunIcon className="icon" />}</button>
         </div>
         <div className="header__right-menu-profile">
           <div className="sign-buttons">
-            <MyRipples className="ripple-container">
-              <button className="sign-in-button">Sign In</button>
-            </MyRipples>
-            <MyRipples className="ripple-container">
-              <button className="sign-up-button">Sign Up</button>
-            </MyRipples>
+            {user ? (
+              <>
+                <span className="profile-info">{user?.displayName}</span>
+                <IconButton onClick={handleLogout} variant="outline" colorScheme="teal" fontSize="20px" icon={<LogoutIcon className="logout-icon" />} />
+              </>
+            ) : (
+              <>
+                <RippleButton buttonType={"secondary"} onClick={() => handleOpen("login")}>
+                  Log In
+                </RippleButton>
+                <RippleButton buttonType={"primary"} onClick={() => handleOpen("signup")}>
+                  Sign Up
+                </RippleButton>
+              </>
+            )}
           </div>
         </div>
       </div>
+      <SearchInput searchVisible={searchVisible} />
     </header>
   );
 };
