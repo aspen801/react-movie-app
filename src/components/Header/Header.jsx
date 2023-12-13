844154bfd35eee2af9c804e96e296d24477fa244
aspen801 <boyko.bogdan45@gmail.com>
2023-12-13 19:57:53 +0200

    header refactor

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,31 +1,33 @@
 import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { Link, useLocation } from "react-router-dom";
-import { setUser } from "../../store/slices/userSlice";
-import RippleButton from "../UI/RippleButton/RippleButton";
 import SearchInput from "../Search/SearchInput/SearchInput";
-import { IconButton } from "@chakra-ui/react";
-import { logOut } from "../../firebase/auth";
+import NavLinks from "./NavLinks/NavLinks";
+import AuthSection from "./AuthSection/AuthSection";
+import BurgerMenu from "./BurgerMenu/BurgerMenu";
+import useWindowSize from "../../hooks/useWindowsSize";
 
 import "./header.scss";
 
 import { themeActions } from "../../store/slices/themeSlice";
-import { setAuthModalOpen, setAuthType } from "../../store/slices/authModalSlice";
 
 import mLogo from "/assets/mlogo.svg";
 import MoonIcon from "../../assets/moon.svg?react";
 import SunIcon from "../../assets/sun.svg?react";
 import SearchIcon from "../../assets/search.svg?react";
-import LogoutIcon from "../../assets/logout.svg?react";
 import CrossIcon from "../../assets/search-close.svg?react";
+import MenuIcon from "../../assets/menu-burger.svg?react";
 
 const Header = () => {
   const dispatch = useDispatch();
   const { user } = useSelector((state) => state.user);
   const { theme } = useSelector((state) => state.theme);
   const [searchVisible, setSearchVisible] = useState(false);
+  const [isMobile, setIsMobile] = useState(false);
+  const [isMenuVisible, setIsMenuVisible] = useState(false);
 
-  let location = useLocation();
+  const location = useLocation();
+  const windowSize = useWindowSize();
 
   useEffect(() => {
     console.log("User changed!");
@@ -35,74 +37,90 @@ const Header = () => {
     dispatch(themeActions.toggleTheme());
   };
 
-  const handleOpen = (authType) => {
-    dispatch(setAuthModalOpen(true));
-    dispatch(setAuthType(authType));
-  };
-
-  const handleLogout = () => {
-    dispatch(setUser(null));
-    logOut();
-  };
-
   useEffect(() => {
     setSearchVisible(false);
+    setIsMenuVisible(false);
   }, [location]);
+
+  useEffect(() => {
+    if (windowSize.width > 800) {
+      setIsMobile(true);
+    } else {
+      setIsMobile(false);
+    }
+  }, [windowSize.width]);
 
   const handleSearchVisible = () => {
     setSearchVisible(!searchVisible);
   };
 
-  return (
-    <header className="header__main-wrapper">
-      <div className="header__left-section">
-        <div className="header__logo-link-container">
-          <Link to={"/"}>
-            <div className="header__logo">
-              <img src={mLogo} alt="" />
-            </div>
-          </Link>
-        </div>
-        <div className="header__nav-links">
-          <RippleButton buttonType={"secondary"} to={"/upcoming"}>
-            Upcoming
-          </RippleButton>
-          <RippleButton buttonType={"secondary"} to={"/movies"}>
-            Movies
-          </RippleButton>
-          <RippleButton buttonType={"secondary"} to={"/series"}>
-            Series
-          </RippleButton>
-        </div>
-      </div>
+  const handleMenuVisible = () => {
+    setIsMenuVisible(!isMenuVisible);
+  };
 
-      <div className="header__right-menu">
-        <div className="header__right-menu-search">
-          <button onClick={handleSearchVisible}>{searchVisible ? <CrossIcon className="icon" /> : <SearchIcon className="icon" />}</button>
-        </div>
-        <div className="header__right-menu-theme-toggle">
-          <button onClick={handleThemeChange}>{theme === "dark" ? <MoonIcon className="icon" /> : <SunIcon className="icon" />}</button>
-        </div>
-        <div className="header__right-menu-profile">
-          <div className="sign-buttons">
-            {user ? (
-              <>
-                <span className="profile-info">{user?.displayName}</span>
-                <IconButton onClick={handleLogout} variant="outline" colorScheme="teal" fontSize="20px" icon={<LogoutIcon className="logout-icon" />} />
-              </>
-            ) : (
-              <>
-                <RippleButton buttonType={"secondary"} onClick={() => handleOpen("login")}>
-                  Log In
-                </RippleButton>
-                <RippleButton buttonType={"primary"} onClick={() => handleOpen("signup")}>
-                  Sign Up
-                </RippleButton>
-              </>
-            )}
+  //TODO: add IconButton component for round buttons
+  // button {
+  //   padding: 8px;
+  //   display: flex;
+  //   justify-content: center;
+  //   align-items: center;
+  //   cursor: pointer;
+  //   background: none;
+  //   border: none;
+  //   z-index: 5;
+  //   &:hover {
+  //     border-radius: 50%;
+  //     background-color: $btn-hover-color;
+  //   }
+  // }
+
+  return (
+    <header>
+      {isMobile ? (
+        // Mobile Layout
+        <div className="header__main-wrapper">
+          <div className="header__left-section">
+            <div className="header__logo-link-container">
+              <Link to={"/"}>
+                <div className="header__logo">
+                  <img src={mLogo} alt="" />
+                </div>
+              </Link>
+            </div>
+            <NavLinks />
+          </div>
+          <div className="header__right-section">
+            <div className="header__right-section-search">
+              <button onClick={handleSearchVisible}>{searchVisible ? <CrossIcon className="icon" /> : <SearchIcon className="icon" />}</button>
+            </div>
+            <div className="header__right-section-theme-toggle">
+              <button onClick={handleThemeChange}>{theme === "dark" ? <MoonIcon className="icon" /> : <SunIcon className="icon" />}</button>
+            </div>
+            <AuthSection />
           </div>
         </div>
-      </div>
+      ) : (
+        // Desktop Layout
+        <div className="header__main-wrapper">
+          <div className="header__left-section">
+            <div className="header__burger-menu-icon">
+              {isMenuVisible ? (
+                <button onClick={handleMenuVisible}>
+                  <CrossIcon className="icon icon-button" />
+                </button>
+              ) : (
+                <button onClick={handleMenuVisible}>
+                  <MenuIcon className="icon icon-button" />
+                </button>
+              )}
+            </div>
+          </div>
+          {isMenuVisible && <BurgerMenu handleThemeChange={handleThemeChange} theme={theme} />}
+          <div className="header__right-section-search">
+            <button onClick={handleSearchVisible}>{searchVisible ? <CrossIcon className="icon" /> : <SearchIcon className="icon" />}</button>
+          </div>
+        </div>
+      )}
       <SearchInput searchVisible={searchVisible} />
     </header>
   );
