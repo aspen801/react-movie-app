e4c9db92d59b9ebe1167cd3696c72f794bde888e
aspen801 <boyko.bogdan45@gmail.com>
2023-12-22 14:59:53 +0200

    function refactor

--- src/hooks/useTimeConvert.js
+++ src/hooks/useTimeConvert.js
@@ -1,9 +1,7 @@
 const useTimeConvert = (num) => {
-  const hours = num / 60;
-  const rhours = Math.floor(hours);
-  const minutes = (hours - rhours) * 60;
-  const rminutes = Math.round(minutes);
-  return `${rhours}h ${rminutes}m`;
+  const hours = Math.floor(num / 60);
+  const minutes = Math.round(num % 60);
+  return `${hours}h ${minutes}m`;
 };
 
 export default useTimeConvert;
